9fa0929f02938d22990868d23ad9237ca70416b7
Kyle Yang <KyleY9@nycstudents.net>
2024-04-19 22:13:41 -0400

    Completed the lab.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 1; i <= 11; i++) {
+            int temp = fibonacci(i);
+            System.out.print(temp + " ");
+        }
+    }
+
+    private static int fibonacci(int x) {
+        if (x == 1) {
+            return 0;
+        }
+        if (x == 2) {
+            return 1;
+        }
+        // double recursive method call
+        return fibonacci(x - 1) + fibonacci(x - 2);
     }
 }
